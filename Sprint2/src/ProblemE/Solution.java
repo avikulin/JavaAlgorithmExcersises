f174bf7e8839f6e8c45372210d44a52b80a9057b
avikulin <asvikulin@mail.ru>
2021-03-13 21:28:31 +0300

    Sprint 2. SolutionE is done.

--- Sprint2/src/ProblemE/Solution.java
+++ Sprint2/src/ProblemE/Solution.java
@@ -14,17 +14,43 @@ class Node<V> {
 
 public class Solution {
 
-    public static Node<String> solution(Node<String> head){
-        Node<String> list = head;
-        while (list != null){
-            Node<String> nextNode = 
+    public static Node<String> solution(Node<String> head) {
+        Node<String> tail = head;
+        Node<String> currentNode = null;
+        Node<String> newHead = null;
+
+        while (tail.next != null) {
+            currentNode = tail.next;
+
+            // выкусываем узел, следующий после tail
+            if (currentNode.next != null) {
+                tail.next = currentNode.next; // ставим прямую ссылку
+                currentNode.next.prev = tail; // ставим обратную ссылку
+            } else {
+                tail.next = null;
+            }
+
+            // отрабатываем начальное условие первого шага
+            if (newHead == null) {
+                newHead = currentNode;
+                newHead.next = tail;
+                newHead.prev = null;
+                tail.prev = newHead;
+            } else {
+                currentNode.next = newHead;
+                newHead.prev = currentNode;
+                newHead = currentNode;
+                newHead.prev = null;
+            }
         }
+
+        return newHead;
     }
 
-    static void printList(Node<String> head){
+    static void printList(Node<String> head) {
         Node<String> currentNode = head;
-        while (currentNode!= null){
-            System.out.println(currentNode.value==null?"null":currentNode.value);
+        while (currentNode != null) {
+            System.out.println(currentNode.value == null ? "null" : currentNode.value);
             currentNode = currentNode.next;
         }
     }
@@ -32,13 +58,16 @@ public class Solution {
     public static void main(String[] args) {
         Node<String> list = new Node<>("0[start]", null, null);
         Node<String> prevNode = list;
-        for (int i=1; i<6; i++){
+        for (int i = 1; i < 6; i++) {
             Node<String> currentNode = new Node<String>(String.format("Element {%d}", i), null, null);
             prevNode.next = currentNode;
             currentNode.prev = prevNode;
             prevNode = currentNode;
         }
         printList(list);
+        System.out.println("----");
+        Node<String> reverse = solution(list);
+        printList(reverse);
     }
 }
 
