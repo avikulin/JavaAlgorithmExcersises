380e0178eca9a8eb558c321a834b92a8a36c4506
avikulin <asvikulin@mail.ru>
2021-04-19 00:09:47 +0300

    Problem A solved

--- Sprint4/src/ProblemA/SolutionA.java
+++ Sprint4/src/ProblemA/SolutionA.java
@@ -1,23 +1,24 @@
-//package ProblemA;
+package ProblemA;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SolutionA {
-    public static int getHash(String str, int base, int modulo) {
-        if (str.length()==0) return 0;
-        if (modulo==1) return 0;
+    public static long getHash(String str, int base, int modulo) {
+        if (str.length() == 0) return 0;
+        if (modulo == 1) return 0;
 
-        int res = str.charAt(0) % modulo;
         base = base % modulo;
+        long res = str.charAt(0);
         for (int i = 1; i < str.length(); i++) {
-            res = ((int) str.charAt(i) % modulo + (res * base) % modulo) % modulo;
+            res = ((int) str.charAt(i) % modulo + (((res % modulo) * base) % modulo)) % modulo;
         }
+
         return res;
     }
 
-    static String process(String[] input) {
+    public static String process(String[] input) {
         int hashingBase = Integer.parseInt(input[0]);
         int hashingModulo = Integer.parseInt(input[1]);
         String strToHash = input[2];
