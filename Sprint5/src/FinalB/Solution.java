1631c05432730c3de421d393bcd5f2e6a8dbc5be
avikulin <asvikulin@mail.ru>
2021-07-01 02:19:14 +0300

    Final B. Semi-ready solution. First 7 tests passed trough.

--- Sprint5/src/FinalB/Solution.java
+++ Sprint5/src/FinalB/Solution.java
@@ -5,21 +5,20 @@ import FinalB.Utils.Node;
 import FinalB.Utils.TreeBuilder;
 import FinalB.Utils.NodeUtils;
 
-import java.util.HashMap;
 import java.util.Stack;
 
-enum Direction {LEFT, RIGHT, ROOT, NULL};
+enum Position {LEFT, RIGHT, ROOT, NULL};
 
-class NodeSearchResults {
+class NodeInfo {
     private Node parent;
     private Node node;
-    private Direction direction;
+    private Position position;
 
 
-    NodeSearchResults(Node parent, Node node, Direction direction) {
+    NodeInfo(Node parent, Node node, Position position) {
         this.node = node;
         this.parent = parent;
-        this.direction = direction;
+        this.position = position;
     }
 
     public Node getNode() {
@@ -30,8 +29,8 @@ class NodeSearchResults {
         return parent;
     }
 
-    public Direction getDirection() {
-        return direction;
+    public Position getPosition() {
+        return position;
     }
 
     public int getNumberOfSuccessors() {
@@ -44,11 +43,11 @@ class NodeSearchResults {
 
     @Override
     public String toString() {
-        return String.format("NodeSearchResults{node = %s, parent=%s, direction=%s, successors = %s}",
+        return String.format("NodeInfo{node = %s, parent=%s, relation to parent=%s, successors = %d}",
                 NodeUtils.nodeToStr(node),
                 NodeUtils.nodeToStr(parent),
-                String.valueOf(direction),
-                String.valueOf(this.getNumberOfSuccessors()));
+                position,
+                this.getNumberOfSuccessors());
     }
 }
 
@@ -56,36 +55,55 @@ public class Solution {
     /**
      * Поиск возможной замены для узла, у которого существует 2 приемника
      *
-     * @param node Заменяемый узел
+     * @param nodeInfo Заменяемый узел
      * @return Найденный приемник
      */
-    private static NodeSearchResults searchForPossibleReplace(Node node) {
-        if (node == null) return new NodeSearchResults(null, null, Direction.NULL);
-        if (node.getRight() == null) return new NodeSearchResults(null, null, Direction.NULL);
+    private static NodeInfo searchForPossibleReplace(NodeInfo nodeInfo) {
+        if (nodeInfo == null) return new NodeInfo(null, null, Position.NULL);
+
         Stack<Node> nodeStack = new Stack<>();
 
-        Node resNode = null;
-        Node resParent = node;
-        Direction direction = Direction.RIGHT;
-        nodeStack.push(node.getRight()); // ишем в правом поддереве.
-        while (!nodeStack.empty()) {
 
-            resNode = nodeStack.pop();
-            if (resNode.getLeft() != null) {
-                nodeStack.push(resNode.getLeft());
-                resParent = resNode;
-                direction = Direction.LEFT;
+        // если у узла нет потомков, - значит нет и приемников. Возвращаем NULL
+        Node resNode = null;
+        Node resParent = null;
+        Position position = Position.NULL;
+
+        // если у узла 1 потомок, - то он и будет приемником.
+        if (nodeInfo.getNumberOfSuccessors() == 1) {
+            if (nodeInfo.getNode().getLeft() == null) {
+                resNode = nodeInfo.getNode().getRight();
+                resParent = nodeInfo.getNode();
+                position = Position.RIGHT;
+            } else {
+                resNode = nodeInfo.getNode().getLeft();
+                resParent = nodeInfo.getNode();
+                position = Position.LEFT;
             }
         }
-        return new NodeSearchResults(resParent, resNode, direction);
+        if (nodeInfo.getNumberOfSuccessors() == 2) {
+            // если у узла 2 потомка - то ищем минимальный элемент в правом поддереве. Он и будет приемником.
+            nodeStack.push(nodeInfo.getNode().getRight());
+            resParent = nodeInfo.getNode();
+            position = Position.RIGHT;
+            while (!nodeStack.empty()) {
+                resNode = nodeStack.pop();
+                if (resNode.getLeft() != null) {
+                    nodeStack.push(resNode.getLeft());
+                    resParent = resNode;
+                    position = Position.LEFT;
+                }
+            }
+        }
+        return new NodeInfo(resParent, resNode, position);
     }
 
-    private static NodeSearchResults searchNode(Node root, int key) {
+    private static NodeInfo searchNode(Node root, int key) {
         Node parent = root;
         Node current = root;
-        Direction direction = Direction.ROOT;
+        Position position = Position.ROOT;
         while (true) {
-            if (current.getValue() == key) return new NodeSearchResults(parent, current, direction); // элемент нашелся.
+            if (current.getValue() == key) return new NodeInfo(parent, current, position); // элемент нашелся.
             if ((current.getLeft() == null) && (current.getRight() == null))
                 return null; // в дереве нет нужного элемента.
             parent = current;
@@ -94,76 +112,79 @@ public class Solution {
                     return null; // в дереве нет нужного элемента.
                 else {
                     current = parent.getLeft();
-                    direction = Direction.LEFT;
+                    position = Position.LEFT;
                 }
             } else {
                 if (parent.getRight() == null)
                     return null; // в дереве нет нужного элемента.
                 else {
                     current = parent.getRight();
-                    direction = Direction.RIGHT;
+                    position = Position.RIGHT;
                 }
             }
 
         }
     }
 
-    private static void replaceNode(Node parentOfNodeToReplace, Direction directionOfNodeToReplace, Node newNode) {
-        if (directionOfNodeToReplace == Direction.LEFT)
-            parentOfNodeToReplace.setLeft(newNode);
-        else
-            parentOfNodeToReplace.setRight(newNode);
-    }
-
-    private static void deleteLeafNode(Node nodeToDelete, Node parentOfNodeToDelete,
-                                       Direction directionOfNodeToDelete) {
-        replaceNode(parentOfNodeToDelete, directionOfNodeToDelete, null);
-
-    }
-
-    private static void cutNodeWithOneChild(Node nodeToDelete, Node parentOfNodeToDelete,
-                                            Direction directionOfNodeToDelete) {
-        Node nodeForReplacement = (nodeToDelete.getLeft() == null) ? nodeToDelete.getRight() : nodeToDelete.getLeft();
-        replaceNode(parentOfNodeToDelete, directionOfNodeToDelete, nodeForReplacement);
-    }
-
-    private static void cutNodeWithTwoChildren(Node nodeToDelete, Node parentOfNodeToDelete,
-                                               Direction directionOfNodeToDelete,
-                                               Node newNode, Node newNodeParent) {
-        Node rightChildOfDeletingNode = nodeToDelete.getRight();
-        Node leftChildOfDeletingNode = nodeToDelete.getLeft();
-        Node rightChildOfNewNode = newNode.getRight();
-
-        replaceNode(parentOfNodeToDelete, directionOfNodeToDelete, newNode);
-        if (rightChildOfDeletingNode == newNode) {       // если newNode является прямым правым потомком nodeToDelete.
-            newNode.setLeft(leftChildOfDeletingNode);
-        } else {                                        // если между newNode и nodeToDelete существует путь.
-            newNodeParent.setLeft(rightChildOfNewNode);
-            newNode.setRight(rightChildOfDeletingNode);
+    private static Node replaceNode(Node root, NodeInfo nodeToReplace, NodeInfo newNode) {
+        if (nodeToReplace.getPosition() == Position.ROOT) {
+            return newNode.getNode();
         }
+
+        if (nodeToReplace.getPosition() == Position.LEFT)
+            nodeToReplace.getParent().setLeft(newNode.getNode());
+        else
+            nodeToReplace.getParent().setRight(newNode.getNode());
+
+        return root;
+    }
+
+    private static Node deleteLeafNode(Node root, NodeInfo nodeToDelete) {
+        return replaceNode(root, nodeToDelete, new NodeInfo(null, null, Position.NULL));
+    }
+
+    private static Node cutNodeWithOneChild(Node root, NodeInfo nodeToDelete, NodeInfo newNode) {
+        return replaceNode(root, nodeToDelete, newNode);
+    }
+
+    private static Node cutNodeWithTwoChildren(Node root, NodeInfo nodeToDelete, NodeInfo newNode) {
+        Node rightChildOfDeletingNode = nodeToDelete.getNode().getRight();
+        Node leftChildOfDeletingNode = nodeToDelete.getNode().getLeft();
+        Node rightChildOfNewNode = newNode.getNode().getRight();
+
+        Node possiblyNewRoot = replaceNode(root, nodeToDelete, newNode);
+        if (rightChildOfDeletingNode == newNode.getNode()) {       // если newNode является прямым правым потомком nodeToDelete.
+            newNode.getNode().setLeft(leftChildOfDeletingNode);
+        } else {                                        // если между newNode и nodeToDelete существует путь.
+            newNode.getParent().setLeft(rightChildOfNewNode);
+            newNode.getNode().setLeft(leftChildOfDeletingNode);
+            newNode.getNode().setRight(rightChildOfDeletingNode);
+        }
+        return possiblyNewRoot;
     }
 
     public static Node remove(Node root, int key) {
-        NodeSearchResults nodeToDelete = searchNode(root, key);
+        Node res = null;
+        NodeInfo nodeToDelete = searchNode(root, key);
         if (nodeToDelete.getNode() == null) return root; //удалять нечего. дерево не изменилось.
         switch (nodeToDelete.getNumberOfSuccessors()) {
-            case 0:
-                    deleteLeafNode(nodeToDelete.getNode(), nodeToDelete.getParent(), nodeToDelete.getDirection());
-                    break;
-            case 1:
-                    cutNodeWithOneChild(nodeToDelete.getNode(), nodeToDelete.getParent(), nodeToDelete.getDirection());
-                    break;
-            case 2:
-                    NodeSearchResults replacementNode= searchForPossibleReplace(nodeToDelete.getNode());
-                    cutNodeWithTwoChildren(nodeToDelete.getNode(),
-                                           nodeToDelete.getParent(),
-                                           nodeToDelete.getDirection(),
-                                           replacementNode.getNode(),
-                                           replacementNode.getParent());
-                    break;
+            case 0: {
+                res = deleteLeafNode(root, nodeToDelete);
+                break;
+            }
+            case 1: {
+                NodeInfo replacementNode = searchForPossibleReplace(nodeToDelete);
+                res = cutNodeWithOneChild(root, nodeToDelete, replacementNode);
+                break;
+            }
+            case 2: {
+                NodeInfo replacementNode = searchForPossibleReplace(nodeToDelete);
+                res = cutNodeWithTwoChildren(root, nodeToDelete, replacementNode);
+                break;
+            }
         }
 
-        return root;
+        return res;
     }
 
     public static String process(TestParamsBundle params) {
@@ -181,13 +202,13 @@ public class Solution {
         );
         System.out.println(String.format("Given tree:\n%s\n", tree.getTreeHierarchyView()));
 
-        int[] keys = new int[]{25};//15, 5, 30, 25, 50};
+        int[] keys = new int[]{15, 5, 30, 25, 35, 40, 50};
         Node[] trees = new Node[keys.length];
 
-        for (int i=0; i<trees.length;i++) {
+        for (int i = 0; i < trees.length; i++) {
             trees[i] = new TreeBuilder(tree.getFlattedTreeValues()).getRootNode();
-            NodeSearchResults searchedNode = searchNode(trees[i], keys[i]);
-            NodeSearchResults replacementNode = searchForPossibleReplace(searchedNode.getNode());
+            NodeInfo searchedNode = searchNode(trees[i], keys[i]);
+            NodeInfo replacementNode = searchForPossibleReplace(searchedNode);
             System.out.println("-----------------------------------------------------------------------------------");
             System.out.println(String.format("Processing key %d:\n\t-%s,\n\t\t-possible replacement:\n\t\t\t-%s\n",
                     keys[i], searchedNode.toString(), replacementNode.toString()));
