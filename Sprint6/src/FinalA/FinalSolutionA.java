ca2f47e3fc305770600b3b23582bf9f5e180adf5
avikulin <asvikulin@mail.ru>
2021-09-28 23:45:16 +0300

    Final problem B. Add text documentation to code. Solution is prepared for CR.

--- Sprint6/src/FinalA/FinalSolutionA.java
+++ Sprint6/src/FinalA/FinalSolutionA.java
@@ -1,10 +1,64 @@
-/*package FinalA;*/
+// Код отправки в Яндекс.Контест - 52659386
+package FinalA; //--- эту строку нужно закомментировать перед отправкой в Яндекс.Контест.
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/*
+    ОПИСАНИЕ РЕШЕНИЯ
+
+    По сути задание сводится к построению максимального остовного дерева, при котором мы на каждой итерации выбираем
+    ребро, инцидентное текущей вершины, но с максимальным весом. Для решения используем алгоритм Прима на базе
+    очереди с приоритетами, которая реализуется классом TreeSet.
+    Для выбора максимального ребра, а также для различимости экземпляров ребер внутри контейнера TreeSet,
+    переопределим метод equals() и compareTo() в классе ребра.
+
+    Решение структурно состоит из следующих классов:
+    1) класс графа <Graph>, реализующего операции добавления ребер, обхода вершин и построения MST
+    2) класс ребра <Edge>, реализующего функционал хранения весов, а также операции сравнения <equals(...)>
+       и упорядочивания <compareTo(...)>, а также маркировка концов ребра, как входящих/не входящих в MST.
+    3) управляющего класса <FinalSolutionA>, реализующего функцию входа в программу <main(...)>
+       и функцию-драйвер <pprocess(...)>
+
+    АНАЛИЗ ПРОСТРАНСТВЕННОЙ И ВРЕМЕННОЙ СЛОЖНОСТИ АЛГОРИТМА
+
+    Алгоритм построения MST (M - максимальное) состоит из n итарций следующих основных этапов:
+
+    А) Формирование перечня ребер, инцидентных текущей вершине, и наполнение очереди с приоритетами.
+       Считаем, что операции сравнения ребер выполняются за O(1) и требуют O(1) дополнительной памяти.
+
+        ВРЕМЕННАЯ СЛОЖНОСТЬ: k*O(log m),
+        ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ (в худшем случае): O(m)
+        где:
+            k - количество ребер, инцидентных текущей вершине. Намного меньше m, поэтому O(k) ~ O(1)
+            m - количество ребер
+
+    Б) Добавление ребра в MST:
+        - извлечение максимального ребра из очереди с приоритетами
+        - проверка ребра на невхождение в MST
+        - маркировка ребра, как входящего в MST
+        - добавление веса ребра к общему весу MST
+
+        ВРЕМЕННАЯ СЛОЖНОСТЬ: O(log m) + O(1) + O(1) + O(1) = O(log m)
+        ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ (в худшем случае): O(1)
+
+    *****
+    ИТОГОВАЯ ВРЕМЕННАЯ СЛОЖНОСТЬ АЛГОРИТМА  (в худшем случае) = n * ({k} * O(log m) + O(log m)) =
+                                                                n * (O(1) * O(log m) + O(log m)) =
+                                                                n * O(2*log m) = O(n*log m)
+
+    ИТОГОВАЯ ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ АЛГОРИТМА  (в худшем случае) = n * (O(m) + O(1)) = n * O(m) = O(n*m)
+
+    где:
+        n - количество вершин,
+        m - количество ребер
+ */
+
+/**
+ * Класс графа
+ */
 class Graph {
     private final List<Edge>[] graphStorage;
     private final boolean[] mstAddedVertexes;
@@ -15,7 +69,12 @@ class Graph {
     private int startVertexId;
     private int numberOfMstVertices;
 
-    public Graph(int vertexCount, int edgesCount, boolean oriented) {
+    /**
+     * Конструктор экземпляра графа
+     * @param vertexCount   Количество вершин
+     * @param oriented  Признак ориентированности. true означает создание ориентированного графа.
+     */
+    public Graph(int vertexCount, boolean oriented) {
         graphStorage = new List[vertexCount + 1];
         mstAddedVertexes = new boolean[vertexCount + 1];
         mstCandidateEdges = new TreeSet<>();
@@ -26,11 +85,15 @@ class Graph {
         mstWeight = 0;
     }
 
-    public void AddEdge(Edge e) {
+    /**
+     * Добавление ребра в граф
+     * @param e Ссылка на экземпляр ребра
+     */
+    public void addEdge(Edge e) {
         int v1 = e.getVertexA();
         int v2 = e.getVertexB();
 
-        if (v1==v2) return; //не добавляем петли в граф. они не влияют на MST.
+        if (v1 == v2) return; //не добавляем петли в граф. они не влияют на MST.
 
         if (graphStorage[v1] == null) {
             graphStorage[v1] = new ArrayList<>();
@@ -45,18 +108,28 @@ class Graph {
         }
     }
 
+    /**
+     * Добавление вершины в MST
+     * @param vertex    Код добавляемой вершины
+     */
     private void addVertexToMST(int vertex) {
         this.mstAddedVertexes[vertex] = true;
         this.numberOfMstVertices++;
         if (this.graphStorage[vertex] == null) return;
         List<Edge> adjVertexes = this.graphStorage[vertex];
+
+        // с удивлением обнаружил, что цикл с итератором на 20% менее эффективен.
+        // и вообще итераторы оказались чудовищно неэффективны, как по времени, так и по памяти.
+        // все бэст-практисы профайлеру под хвост.... когда бьешься за миллисекунды - уже не до красоты.
+
         /*for (Edge e : adjVertexes) {
             if (!this.mstAddedVertexes[e.getOppositeVertex(vertex)]) {
                 e.setMSTIncludedVertex(vertex);
                 this.mstCandidateEdges.add(e);
             }
         }*/
-        for (int i=0; i<adjVertexes.size();i++) {
+
+        for (int i = 0; i < adjVertexes.size(); i++) {
             Edge e = adjVertexes.get(i);
             if (!this.mstAddedVertexes[e.getOppositeVertex(vertex)]) {
                 e.setMSTIncludedVertex(vertex);
@@ -65,10 +138,18 @@ class Graph {
         }
     }
 
+    /**
+     * Задание стартовой вершины, начиная с которой производится построение MST
+     * @param vertex    Код стартовой вершины
+     */
     public void setStartVertex(int vertex) {
         this.startVertexId = vertex;
     }
 
+    /**
+     * Выполнение обхода графа вглубину и построение MST
+     * @return  Признак полноты MST. true означает, что MST содержит все вершины графа.
+     */
     public boolean traverseMst() {
         addVertexToMST(startVertexId);
         while ((this.numberOfMstVertices < this.numberOfVertexes) && (!this.mstCandidateEdges.isEmpty())) {
@@ -83,18 +164,31 @@ class Graph {
         return this.numberOfVertexes == this.numberOfMstVertices;
     }
 
+    /**
+     * Получение значения итогового веса MST
+     * @return  Значение веса MST
+     */
     public int getMstWeight() {
         return this.mstWeight;
     }
 }
 
+/**
+ * Класс ребра графа
+ */
 class Edge implements Comparable<Edge> {
-    private int vertexA;
-    private int vertexB;
+    private final int vertexA;
+    private final int vertexB;
     private boolean vertexAinMST;
     private boolean vertexBinMST;
-    private int weight;
+    private final int weight;
 
+    /**
+     * Конструктор экземпляра ребра
+     * @param vertexA   Код вершины А
+     * @param vertexB   Код вершины B
+     * @param weight    Вес ребра
+     */
     public Edge(int vertexA, int vertexB, int weight) {
         this.vertexA = vertexA;
         this.vertexB = vertexB;
@@ -103,14 +197,27 @@ class Edge implements Comparable<Edge> {
         this.vertexBinMST = false;
     }
 
+    /**
+     * Получение кода вершины-начала ребра
+     * @return  Код начальной вершины
+     */
     public int getVertexA() {
         return this.vertexA;
     }
+
+    /**
+     * Получение кода вершины-конца ребра
+     * @return  Код конечной вершины
+     */
 
     public int getVertexB() {
         return this.vertexB;
     }
 
+    /**
+     * Маркировка заданной вершины ребра, как входящей в MST
+     * @param vertex    Код вершины
+     */
     public void setMSTIncludedVertex(int vertex) {
         if (vertex == vertexA)
             this.vertexAinMST = true;
@@ -118,37 +225,64 @@ class Edge implements Comparable<Edge> {
             this.vertexBinMST = true;
     }
 
+    /**
+     * Получение кода вершины, противоположной заданной
+     * @param vertex    Код заданной вершины
+     * @return  Код противоположной вершины
+     */
     public int getOppositeVertex(int vertex) {
         return (vertex == vertexA) ? vertexB : vertexA;
     }
 
+    /**
+     * Получение кода вершины одного из концов ребра, не включенной в MST
+     * @return  Код вершины ребра, не включенной в MST
+     */
     public int getNotMSTVertex() {
         if ((!this.vertexAinMST) && (!this.vertexBinMST)) return -1;
         return (this.vertexAinMST) ? this.vertexB : this.vertexA;
     }
 
+    /**
+     * Получение веса ребра
+     * @return  Значение веса
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     * Проверка равенства экзепляров класса ребра графа
+     * @param o Ссылка на экземпляр ребра, сравниваемый с данным
+     * @return  Результат проверки. true означает эквивалентность экземпляров.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         Edge edge = (Edge) o;
-        /*boolean vertexesAreTheSame = ((this.vertexA == edge.vertexB)&&(this.vertexB == edge.vertexA));
-        boolean vertexesAreEqual = ((this.vertexA == edge.vertexA)&&(this.vertexB == edge.vertexB));
-        boolean weightsAreEqual = (this.weight == edge.weight);
-        return (vertexesAreEqual||vertexesAreTheSame)&&weightsAreEqual;*/
+
         return vertexA == edge.vertexA &&
                 vertexB == edge.vertexB &&
                 weight == edge.weight;
     }
 
+    /**
+     * Получение хэщ-кода экземпляра ребра (необходим для корректной работы контейнера TreeSet).
+     * @return Значение хэш-кода.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(vertexA, vertexB, weight);
     }
 
+    /**
+     * Выполнение сравнения текущего экземпляра ребра с другим.
+     * @param o Ссылка на сравниваемый экземпляр ребра
+     * @return Результат сравнения:
+     *                              0 - экземпляры равны,
+     *                              1 - текущий экземпляр больше,
+     *                              (-1) - сравниваемый экземпляр больше
+     */
     @Override
     public int compareTo(Edge o) {
         if (this.equals(o)) return 0;
@@ -160,33 +294,42 @@ class Edge implements Comparable<Edge> {
     }
 }
 
+/**
+ * Основной класс выполнения расчетов
+ */
 public class FinalSolutionA {
 
+    /**
+     * Функция-драйвер
+     * @param input Массив входных строк (согласно условию задачи)
+     * @return  Выходная строка (согласно условию задачи)
+     * @throws RuntimeException
+     */
     public static String process(String[] input) throws RuntimeException {
         String mainParams = input[0];
         StringTokenizer mainParamsTokens = new StringTokenizer(mainParams);
 
         int vertexCount = Integer.parseInt(mainParamsTokens.nextToken());
         int edgeCount = Integer.parseInt(mainParamsTokens.nextToken());
-        mainParamsTokens = null;
+        mainParamsTokens = null; //обжогшись на строках, дую на null
 
-        Graph graph = new Graph(vertexCount, edgeCount, false);
+        Graph graph = new Graph(vertexCount, false);
         for (int i = 1; i < edgeCount + 1; i++) {
+            // когда возвращаются не только токены данных, но и разделители,
+            // StringTokenizer работает на 20% эффективнее по времени.
             StringTokenizer edgeParamsTokens = new StringTokenizer(input[i], " ", true);
             int vertexA = Integer.parseInt(edgeParamsTokens.nextToken());
             String spaceA = edgeParamsTokens.nextToken();
             int vertexB = Integer.parseInt(edgeParamsTokens.nextToken());
             String spaceB = edgeParamsTokens.nextToken();
             int weight = Integer.parseInt(edgeParamsTokens.nextToken());
+
+            //принудительно подчищаем память между итерациями
             edgeParamsTokens = null;
             spaceA = null;
             spaceB = null;
-            /*String[] tokens = input[i].split("\\s");
-            int vertexA = Integer.parseInt(tokens[0]);
-            int vertexB = Integer.parseInt(tokens[1]);
-            int weight = Integer.parseInt(tokens[2]);
-            tokens = null;*/
-            graph.AddEdge(new Edge(vertexA, vertexB, weight));
+
+            graph.addEdge(new Edge(vertexA, vertexB, weight));
 
         }
         graph.setStartVertex(1);
@@ -194,97 +337,12 @@ public class FinalSolutionA {
         return (res) ? Integer.toString(graph.getMstWeight()) : "Oops! I did it again";
     }
 
-    /*public static void profile(String[] input) {
-        while (true) {
-            process(input);
-        }
-    }*/
-
+    /**
+     * Функция входа в программу
+     * @param args  Аргументы командной строки (для целей совместимости)
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        /*profile(new String[]{
-                "1000 80",
-                "143 293 8219",
-                "532 82 4567",
-                "911 734 6423",
-                "80 568 6910",
-                "765 447 9126",
-                "169 952 6911",
-                "541 696 948",
-                "856 180 3606",
-                "815 774 8218",
-                "758 847 6739",
-                "698 859 741",
-                "989 920 2694",
-                "981 571 1371",
-                "644 615 2295",
-                "342 537 3261",
-                "597 775 7800",
-                "828 975 4900",
-                "844 535 2118",
-                "763 275 2055",
-                "336 836 4126",
-                "855 399 7546",
-                "329 964 4355",
-                "608 809 160",
-                "996 564 9943",
-                "275 197 4096",
-                "8 260 9317",
-                "751 113 9817",
-                "806 905 9372",
-                "809 346 3215",
-                "142 662 5664",
-                "588 903 4252",
-                "388 852 9277",
-                "331 9 4828",
-                "404 812 2096",
-                "930 564 8416",
-                "911 731 1727",
-                "867 40 5105",
-                "586 950 3977",
-                "715 763 3848",
-                "516 302 400",
-                "132 95 4552",
-                "371 490 1223",
-                "33 843 7032",
-                "678 955 9683",
-                "172 433 8854",
-                "213 292 8802",
-                "132 918 5563",
-                "227 347 9233",
-                "871 34 8345",
-                "702 477 8609",
-                "6 108 5507",
-                "320 924 1087",
-                "361 442 1056",
-                "117 656 8911",
-                "583 637 7921",
-                "350 812 1387",
-                "324 969 114",
-                "533 424 8847",
-                "43 774 1046",
-                "83 286 3369",
-                "777 536 1443",
-                "980 993 9123",
-                "332 807 8748",
-                "485 639 2023",
-                "305 480 3525",
-                "950 512 4132",
-                "58 832 3765",
-                "597 552 8485",
-                "420 970 6739",
-                "164 402 6107",
-                "472 825 3306",
-                "815 369 4911",
-                "375 202 334",
-                "936 235 7701",
-                "51 429 3761",
-                "503 65 9565",
-                "602 83 7378",
-                "712 358 568",
-                "281 678 5069",
-                "944 524 71"
-        });*/
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String params = reader.readLine();
         StringTokenizer mainParamsTokens = new StringTokenizer(params);
